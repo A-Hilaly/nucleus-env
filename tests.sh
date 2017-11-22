41ee0d1dc29fac70d0387ed4f3fee185e7238b0d
a-hilaly <hilalyamine@gmail.com>
2017-11-22 01:28:07 +0100

    Add test file

---
+++ tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+function check_nucleus_main_dirs () {
+
+}
+
+function check_nucleus_venv () {
+
+}
+
+function check_bash_profile () {
+
+}
+
+function test_checkers () {
+
+}
+
+function test_nls_export () {
+
+}
+
+function test_nls_export () {
+
+}
+
+function test_nls_ls () {
+
+}
+
+function test_nls_clear () {
+
+}
